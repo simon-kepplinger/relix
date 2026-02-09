6325603d6f860caed32540ab165871616066aed8
Simon Kepplinger <simon.kepplinger@valcome.at>
2026-02-10 00:57:34 +0100

    feat(commands): add `EX` and `PX` support to `SET` command

--- lib/relix/commands/set.ex
+++ lib/relix/commands/set.ex
@@ -2,6 +2,28 @@ defmodule Relix.Commands.Set do
   def dispatch([key, value]) do
     true = Relix.Store.set(key, value)
 
+    ack()
+  end
+
+  def dispatch([key, value, command, arg]) do
+    dispatch([key, value], [String.upcase(command), arg])
+  end
+
+  def dispatch([key, value], ["PX", ms_str]) do
+    milliseconds = :erlang.binary_to_integer(ms_str)
+    true = Relix.Store.set(key, value, milliseconds)
+
+    ack()
+  end
+
+  def dispatch([key, value], ["EX", s_str]) do
+    seconds = :erlang.binary_to_integer(s_str)
+    true = Relix.Store.set(key, value, seconds * 1_000)
+
+    ack()
+  end
+
+  defp ack() do
     {:reply, "+OK\r\n"}
   end
 end
--- lib/relix/store.ex
+++ lib/relix/store.ex
@@ -6,7 +6,13 @@ defmodule Relix.Store do
   end
 
   def init(_) do
-    :ets.new(__MODULE__, [:named_table, :set, :public, read_concurrency: true])
+    :ets.new(__MODULE__, [
+      :named_table,
+      :set,
+      :public,
+      read_concurrency: true,
+      write_concurrency: true
+    ])
 
     {:ok, nil}
   end
@@ -15,10 +21,27 @@ defmodule Relix.Store do
     :ets.insert(__MODULE__, {key, value})
   end
 
+  def set(key, value, ttl_ms) do
+    :ets.insert(__MODULE__, {key, value, now() + ttl_ms})
+  end
+
   def get(key) do
     case :ets.lookup(__MODULE__, key) do
       [{^key, value}] -> value
+      [{^key, value, exp}] -> check_exp({key, value, exp})
       _ -> nil
     end
   end
+
+  defp check_exp({key, value, exp}) do
+    if now() < exp do
+      value
+    else
+      :ets.delete(__MODULE__, key)
+      nil
+    end
+  end
+
+  defp now(),
+    do: System.monotonic_time(:millisecond)
 end
